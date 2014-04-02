ab5fc69f829385245ca9d27b9354ddff5a7eb437
botheredbybees <botheredbybees@gmail.com>
2014-04-02 22:13:07 +1100

    load up node objects and display network on screen

--- html/app/scripts/main.js
+++ html/app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+function node(name,image,asset)
+{
+	// make asset an optional argument as most tiles don't have one on them
+	// the next line will set the asset to an empty string if it hasn't been passed in the object constructor function call
+	asset = typeof asset !== 'undefined' ? asset : ''; 
+	this.name=name;
+	this.image=image;
+	this.compromised=false;
+	this.loot=new Array();
+	this.asset=asset;
+}
+
+var network = new Array(new node('Backup File Server', 'node_backup_file_server'),
+new node('Certificate Sevices', 'node_cert_sevices'),
+new node('Chat Server', 'node_chat_server'),
+new node('Laptop Client', 'node_client_laptop'),
+new node('Mobile Client', 'node_client_mobile'),
+new node('Tablet Client', 'node_client_tablet'),
+new node('Tower Client', 'node_client_tower'),
+new node('Customer Database', 'node_customer_db'),
+new node('Firewall', 'node_firewall'),
+new node('IMAP Server', 'node_imap_server'),
+new node('Intrusion Detection System', 'node_intrusion_sys'),
+new node('NAT Device', 'node_nat_device'),
+new node('Network File Server', 'node_network_file_server'),
+new node('Primary DNS', 'node_primary_dns'),
+new node('Sales Database', 'node_sales_db'),
+new node('Secondary DNS', 'node_secondary_dns'),
+new node('SMTP Server', 'node_smtp_server'),
+new node('SSO Service', 'node_sso_service'),
+new node('VLAN Switch', 'node_vlan_switch'),
+new node('VOIP Server', 'node_voip_server'),
+new node('VPN Gateway', 'node_vpn_gateway'),
+new node('Web Server', 'node_web_server'),
+new node('Wireless Router', 'node_wireless_router'),
+new node('Internet Gateway', 'node_internet_gateway'));
+
+$( document ).ready(function() {
+    // load up the tiles
+    for(i=0;i<network.length;++i) {
+    	$('#tile'+(i+1)).find('img').attr('src','img/tiles/'+network[i].image+'.png');
+    	$('#tile'+(i+1)).find('img').attr('alt',network[i].name);
+
+    }
+});
